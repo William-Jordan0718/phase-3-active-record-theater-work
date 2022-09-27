09b0b1aea5c14a0874ccf0d0daaa9aa470c1c8e3
eric jubera <ericjubera@gmail.com>
2022-09-27 10:54:22 -0500

    finsh

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,3 +1,23 @@
 class Role < ActiveRecord::Base
   has_many :auditions
+  def actors
+    self.auditions.map{|audition|audition.actor}
+  end
+
+  def locations 
+    self.auditions.map{|audition|audition.location}
+  end 
+
+
+  def lead
+self.auditions.find_by hired:true ||  'no actor has been hired for this role'
+  end 
+
+def understudy
+ 
+ auditions.where(hired:true).second || 'no actor has been hired for understudy for this role'
+
+end 
+
+
 end
